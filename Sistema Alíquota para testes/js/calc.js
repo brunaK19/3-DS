051b3a09c917393d04ec5d014cf9758f0ee2555a
Bruna Karen <114186097+brunaK19@users.noreply.github.com>
2023-03-20 17:37:51 -0300

    Update calc.js

--- Sistema Alíquota para testes/js/calc.js
+++ Sistema Alíquota para testes/js/calc.js
@@ -12,6 +12,30 @@ const Rendimento = document.getElementById("rendimento")
 const Aliquota = document.getElementById("aliquota")
 const Termo = document.getElementById("termo")
 
+//verificar se o CPF é valido
+function validarCPF(){
+    var sum = 0;
+    var rest;
+    var i;
+    var cpf = CPF;
+    cpf = cpf.replace(/\./g, '');
+    cpf = cpf.replace('-', '');
+    cpf = cpf.split('');
+
+    for (i=1; i<=9; i++) sum = sum + parseInt(cpf[i-1]) * (11 - i);
+    rest = (sum * 10) % 11;
+
+    if ((rest == 10) || (rest == 11))  rest = 0;
+    if (rest != parseInt(cpf[9])) return false;
+
+    sum = 0;
+    for (i = 1; i <= 10; i++) sum = sum + parseInt(cpf[i-1]) * (12 - i);
+    rest = (sum * 10) % 11;
+
+    if ((rest == 10) || (rest == 11))  rest = 0;
+    if (rest != parseInt(cpf[10])) return false;
+    return true;
+}
 
 let aliquota;
 
@@ -36,6 +60,11 @@ else if ( rendimento > 55976.16 ) {
 }
 
 let termo = (aliquota/100)*rendimento;
+
+if (validarCPF() == false){
+    alert("CPF inválido");
+    window.location.href = "index.html";
+}
 
 Nome.textContent = nome
 Cpf.textContent = CPF
