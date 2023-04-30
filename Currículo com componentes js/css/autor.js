2a66d68562470b3b8d956a410356d3fb72325e6b
Bruna Karen <114186097+brunaK19@users.noreply.github.com>
2023-04-30 18:46:54 -0300

    Create autor.js

---
+++ Currículo com componentes js/css/autor.js
@@ -0,0 +1,57 @@
+*{
+    margin: auto;
+    
+}
+
+body {
+    background:#9F7DFF;
+   
+}
+
+
+.container{
+    background-color: white;
+    border-radius: 30px 30px 30px 30px;
+    display: block;
+    justify-content: center;
+    text-align: center;
+    width: 30%; 
+    height: 70vh;
+    margin-top: 5%;
+    border-style:double;
+    border-color: #7738F4;
+    border-width: 1ch;
+}
+
+img{
+    margin: 10%;
+    border-radius: 50%;
+    height: 40%;
+    text-align: center;
+}
+
+h1{
+    font-family:'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+    color:#7738F4;
+}
+
+p{
+    
+    margin: 5%;
+    font-size:120%;
+    text-align: left;
+    margin-left: 30px;
+    color:black;
+    font-family:'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+}
+
+a{
+    padding: 8px 10px;
+    color:white;
+    text-decoration: none;
+    text-align: center;
+    font-size: 25px;
+    display:block;
+    margin: auto;
+    font-family:'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+}
