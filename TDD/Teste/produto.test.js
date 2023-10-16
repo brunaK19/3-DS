debf3b4ea55260be557f68ea90356e67fc9d7ba6
Bruna Karen <114186097+brunaK19@users.noreply.github.com>
2023-10-15 23:25:47 -0300

    Update produto.test.js

--- TDD/Teste/produto.test.js
+++ TDD/Teste/produto.test.js
@@ -1,11 +1,12 @@
 const Produto = require('./Produto/produto');
 
 test('Deve criar um produto com atributos corretos', () => {
-  const produto = new Produto('Produto A', 50, 'Descrição do Produto', '123', 'Fabricante XYZ');
+  const produto = new Produto('Produto A', 50, 'Quadrado', '123', 'Fabricante XYZ');
 
   expect(produto.nome).toBe('Produto A');
   expect(produto.precoMinimo).toBe(50);
-
+  expect(produto.descricao).toBe('Quadrado');
+   expect(produto.fabricante).toBe('Fabricante XYZ');
 });
 
 
