9a0ede165d9e02eeccb3e285be26406d0ef50aa2
Bruna Karen <114186097+brunaK19@users.noreply.github.com>
2023-10-15 23:19:21 -0300

    Update leilao.test.js

--- TDD/Teste/leilao.test.js
+++ TDD/Teste/leilao.test.js
@@ -1,6 +1,6 @@
 const Leilao = require('./Produto/leilao');
 
-test('Deve retornar os 3 maiores lances', () => {
+test('retornar os 3 maiores lances', () => {
   const leilao = new Leilao();
   leilao.darLance(100);
   leilao.darLance(150);
@@ -11,4 +11,15 @@ test('Deve retornar os 3 maiores lances', () => {
   const topThreeBids = leilao.getTopThreeBids();
 
   expect(topThreeBids).toEqual([200, 180, 150]);
+});
+
+test('calcular a média dos lances', () => {
+  const leilao = new Leilao();
+  leilao.darLance(100);
+  leilao.darLance(150);
+  leilao.darLance(120);
+
+  const media = leilao.calcularMediaDosLances();
+
+  expect(media).toBe(123.33); // Valor aproximado
 });
